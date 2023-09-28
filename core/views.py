314bb069481f39021ccc94e7b6ac0a059585d146
REINAEL <rbyabut@student,hau,edu,ph>
2023-09-28 12:23:18 +0800

    Message

--- core/views.py
+++ core/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from persons.models import Doctor, Patient
+from .forms import SignupForm
+from django.urls import reverse_lazy
 
 
 
 # Create your views here.
 def index(request):
-    persons = Doctor.objects.all()
-    return render(request, 'core/index.html',
-                {'doctors': Doctor, 'patients': Patient,}
+    doctors = Doctor.objects.all()
+    return render(request, 'persons/doctors.html',
+                {'doctors': doctors, 'patients': Patient,}
                   )
 def contact(request):
     return render(request, 'core/contact.html'
                   )
+
+def doctor(request, doctors=None):
+    doctor = Doctor.objects.all()
+    return render(request, 'persons/doctors.html',
+                  {'doctors': doctors, 'patients': Patient,}
+                  )
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = SignupForm()
+    return render(request, 'core/signup.html', {'form': form})
+
--- dpuddle/urls.py
+++ dpuddle/urls.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from core.views import index, contact
+from django.urls import path , include
+from core.views import index, contact, doctor
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('contact/', contact, name='contact'),
-    path('', index, name='index'),
+    path('', include('core.urls')),
+    path('doctor/', doctor, name='doctor'),
     path('admin/', admin.site.urls),
     path('about/', index, name='about'),
-]# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# if settings.DEBUG:
-#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
